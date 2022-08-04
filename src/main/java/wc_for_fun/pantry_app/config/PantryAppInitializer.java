ba0b2aa6821a33ed1e35e0955d50a79ba1d3aacf
emitacek <mitacek2@hotmail.com>
2022-08-04 11:55:35 -0500

    Maybe this will run properly on tomcat

--- src/main/java/wc_for_fun/pantry_app/config/PantryAppInitializer.java
+++ src/main/java/wc_for_fun/pantry_app/config/PantryAppInitializer.java
@@ -1,28 +1,28 @@
-//package wc_for_fun.pantry_app.config;
-//
-//import javax.servlet.ServletContext;
-//import javax.servlet.ServletException;
-//import javax.servlet.ServletRegistration;
-//
-//import org.springframework.web.WebApplicationInitializer;
-//import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-//import org.springframework.web.servlet.DispatcherServlet;
-//
-//public class PantryAppInitializer implements WebApplicationInitializer {
-//
-//	@Override
-//	public void onStartup(ServletContext servletContext) throws ServletException {
-//		AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
-//		webContext.setConfigLocation("wc_for_fun.panty_app.config");
-//		
-//		webContext.register(MvcConfiguration.class);
-//		webContext.setServletContext(servletContext);
-//
-//		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher2", new DispatcherServlet(webContext));
-//
-//		servlet.setLoadOnStartup(1);
-//		servlet.addMapping("/");
-//
-//	}
-//
-//}
+package wc_for_fun.pantry_app.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class PantryAppInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+		webContext.setConfigLocation("wc_for_fun.panty_app.config");
+		
+		webContext.register(MvcConfiguration.class);
+		webContext.setServletContext(servletContext);
+
+		ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher2", new DispatcherServlet(webContext));
+
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+
+	}
+
+}
